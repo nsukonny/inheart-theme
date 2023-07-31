ac6d07ae4a120f740ecbdc2df229dd6d9b5ab8e7
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-08-01 00:01:13 +0300

    test: turn back

--- template-parts/new-memory/step-0/step-0.php
+++ template-parts/new-memory/step-0/step-0.php
@@ -14,17 +14,6 @@ $is_active		= ( isset( $args['is_active'] ) && $args['is_active'] == 'true' ) ? 
 $title			= get_field( 'title_0' );
 $desc			= get_field( 'desc_0' );
 $themes_desc	= get_field( 'themes_desc' );
-
-echo 'test top<br />';
-$binaries_arr = [
-	'ffmpeg.binaries'  => THEME_DIR . '/lib-php/ffmpeg.exe',
-	'ffprobe.binaries' => THEME_DIR . '/lib-php/ffprobe.exe'
-];
-print_r($binaries_arr);
-$ffprobe	= FFMpeg\FFProbe::create( $binaries_arr );
-var_dump($ffprobe);
-$duration	= ( int ) $ffprobe->format( get_attached_file( 181 ) )->get( 'duration' );
-echo 'test - ', $duration;
 ?>
 
 <section id="new-memory-step-0" class="new-memory-step new-memory-step-0 direction-column<?php echo esc_attr( $is_active ) ?>">
