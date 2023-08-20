101c2dd5a82e7269e627199c48e110c8354dabed
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-08-20 21:23:53 +0300

    feat: menu button in process

---
+++ page-templates/profile.php
@@ -0,0 +1,268 @@
+<?php
+
+/**
+ * Template name: Profile
+ *
+ * @package WordPress
+ * @subpackage inheart
+ */
+
+get_header();
+
+wp_enqueue_style( 'profile', THEME_URI . '/static/css/pages/profile.min.css', [], THEME_VERSION );
+wp_enqueue_script( 'profile', THEME_URI . '/static/js/profile/profile.min.js', [], THEME_VERSION, true );
+?>
+
+<main class="main profile">
+	<div class="container">
+		<section>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorum quaerat ut veniam? Animi
+				aspernatur blanditiis, commodi dolorum error labore, nihil quae quas quia quibusdam tempore temporibus vero.
+				Animi, aperiam?
+			</div>
+			<div>Aspernatur culpa distinctio dolore et fuga, incidunt libero molestiae mollitia nostrum nulla provident quo.
+				At cum cupiditate dicta ea ex fugiat, fugit, illum incidunt laudantium maiores, quis sequi similique vel.
+			</div>
+			<div>Cumque impedit pariatur quas! Ad aliquam aspernatur delectus doloremque eveniet exercitationem id ipsam
+				ipsum, laudantium neque nisi nostrum numquam, odit officiis reprehenderit rerum sequi, sint temporibus
+				tenetur totam unde voluptates.
+			</div>
+			<div>Atque ipsa magnam nisi nobis officia praesentium repellat soluta velit? Eos ex ipsam ipsum unde veniam.
+				Autem cum dicta fugiat maiores optio unde. Dignissimos eius illum inventore, nobis officiis tempore?
+			</div>
+			<div>Adipisci eius facilis hic nam non pariatur totam. Aliquam autem dicta dolor fugiat impedit, labore
+				laboriosam libero minus molestiae mollitia, nam nulla perspiciatis provident quaerat quia quisquam sapiente
+				ut vero.
+			</div>
+			<div>Accusantium adipisci animi consequatur corporis cumque debitis dolor dolore enim eum expedita explicabo
+				facilis incidunt iusto labore natus neque nesciunt non officia perferendis praesentium quia quibusdam sunt
+				velit vero, voluptatem!
+			</div>
+			<div>A accusamus accusantium aspernatur, at, commodi cum cupiditate deserunt dignissimos eaque eligendi enim
+				facilis fuga illum itaque, laudantium minima molestiae neque nisi nostrum odit provident quis quisquam rerum
+				sequi velit!
+			</div>
+			<div>Ab accusantium alias asperiores, autem debitis dolores est expedita ipsum itaque modi necessitatibus nisi
+				provident quae quas qui ratione rem repellendus sed suscipit tenetur ut vitae, voluptatem voluptates.
+				Delectus, fugiat?
+			</div>
+			<div>Corporis cum cumque dolores eligendi ipsa obcaecati, veritatis. Consequuntur deleniti harum impedit
+				inventore, magnam natus non obcaecati officiis quo rerum. Amet at commodi dignissimos distinctio eaque
+				eveniet, ipsa provident vero!
+			</div>
+			<div>Adipisci alias architecto asperiores corporis, ducimus eligendi enim facilis id illum laboriosam magnam
+				molestias mollitia necessitatibus nesciunt numquam obcaecati pariatur, perspiciatis quisquam quod ratione
+				reprehenderit sapiente sequi soluta, tenetur vel?
+			</div>
+		</section>
+
+		<section>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorum quaerat ut veniam? Animi
+				aspernatur blanditiis, commodi dolorum error labore, nihil quae quas quia quibusdam tempore temporibus vero.
+				Animi, aperiam?
+			</div>
+			<div>Aspernatur culpa distinctio dolore et fuga, incidunt libero molestiae mollitia nostrum nulla provident quo.
+				At cum cupiditate dicta ea ex fugiat, fugit, illum incidunt laudantium maiores, quis sequi similique vel.
+			</div>
+			<div>Cumque impedit pariatur quas! Ad aliquam aspernatur delectus doloremque eveniet exercitationem id ipsam
+				ipsum, laudantium neque nisi nostrum numquam, odit officiis reprehenderit rerum sequi, sint temporibus
+				tenetur totam unde voluptates.
+			</div>
+			<div>Atque ipsa magnam nisi nobis officia praesentium repellat soluta velit? Eos ex ipsam ipsum unde veniam.
+				Autem cum dicta fugiat maiores optio unde. Dignissimos eius illum inventore, nobis officiis tempore?
+			</div>
+			<div>Adipisci eius facilis hic nam non pariatur totam. Aliquam autem dicta dolor fugiat impedit, labore
+				laboriosam libero minus molestiae mollitia, nam nulla perspiciatis provident quaerat quia quisquam sapiente
+				ut vero.
+			</div>
+			<div>Accusantium adipisci animi consequatur corporis cumque debitis dolor dolore enim eum expedita explicabo
+				facilis incidunt iusto labore natus neque nesciunt non officia perferendis praesentium quia quibusdam sunt
+				velit vero, voluptatem!
+			</div>
+			<div>A accusamus accusantium aspernatur, at, commodi cum cupiditate deserunt dignissimos eaque eligendi enim
+				facilis fuga illum itaque, laudantium minima molestiae neque nisi nostrum odit provident quis quisquam rerum
+				sequi velit!
+			</div>
+			<div>Ab accusantium alias asperiores, autem debitis dolores est expedita ipsum itaque modi necessitatibus nisi
+				provident quae quas qui ratione rem repellendus sed suscipit tenetur ut vitae, voluptatem voluptates.
+				Delectus, fugiat?
+			</div>
+			<div>Corporis cum cumque dolores eligendi ipsa obcaecati, veritatis. Consequuntur deleniti harum impedit
+				inventore, magnam natus non obcaecati officiis quo rerum. Amet at commodi dignissimos distinctio eaque
+				eveniet, ipsa provident vero!
+			</div>
+			<div>Adipisci alias architecto asperiores corporis, ducimus eligendi enim facilis id illum laboriosam magnam
+				molestias mollitia necessitatibus nesciunt numquam obcaecati pariatur, perspiciatis quisquam quod ratione
+				reprehenderit sapiente sequi soluta, tenetur vel?
+			</div>
+		</section>
+
+		<section>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorum quaerat ut veniam? Animi
+				aspernatur blanditiis, commodi dolorum error labore, nihil quae quas quia quibusdam tempore temporibus vero.
+				Animi, aperiam?
+			</div>
+			<div>Aspernatur culpa distinctio dolore et fuga, incidunt libero molestiae mollitia nostrum nulla provident quo.
+				At cum cupiditate dicta ea ex fugiat, fugit, illum incidunt laudantium maiores, quis sequi similique vel.
+			</div>
+			<div>Cumque impedit pariatur quas! Ad aliquam aspernatur delectus doloremque eveniet exercitationem id ipsam
+				ipsum, laudantium neque nisi nostrum numquam, odit officiis reprehenderit rerum sequi, sint temporibus
+				tenetur totam unde voluptates.
+			</div>
+			<div>Atque ipsa magnam nisi nobis officia praesentium repellat soluta velit? Eos ex ipsam ipsum unde veniam.
+				Autem cum dicta fugiat maiores optio unde. Dignissimos eius illum inventore, nobis officiis tempore?
+			</div>
+			<div>Adipisci eius facilis hic nam non pariatur totam. Aliquam autem dicta dolor fugiat impedit, labore
+				laboriosam libero minus molestiae mollitia, nam nulla perspiciatis provident quaerat quia quisquam sapiente
+				ut vero.
+			</div>
+			<div>Accusantium adipisci animi consequatur corporis cumque debitis dolor dolore enim eum expedita explicabo
+				facilis incidunt iusto labore natus neque nesciunt non officia perferendis praesentium quia quibusdam sunt
+				velit vero, voluptatem!
+			</div>
+			<div>A accusamus accusantium aspernatur, at, commodi cum cupiditate deserunt dignissimos eaque eligendi enim
+				facilis fuga illum itaque, laudantium minima molestiae neque nisi nostrum odit provident quis quisquam rerum
+				sequi velit!
+			</div>
+			<div>Ab accusantium alias asperiores, autem debitis dolores est expedita ipsum itaque modi necessitatibus nisi
+				provident quae quas qui ratione rem repellendus sed suscipit tenetur ut vitae, voluptatem voluptates.
+				Delectus, fugiat?
+			</div>
+			<div>Corporis cum cumque dolores eligendi ipsa obcaecati, veritatis. Consequuntur deleniti harum impedit
+				inventore, magnam natus non obcaecati officiis quo rerum. Amet at commodi dignissimos distinctio eaque
+				eveniet, ipsa provident vero!
+			</div>
+			<div>Adipisci alias architecto asperiores corporis, ducimus eligendi enim facilis id illum laboriosam magnam
+				molestias mollitia necessitatibus nesciunt numquam obcaecati pariatur, perspiciatis quisquam quod ratione
+				reprehenderit sapiente sequi soluta, tenetur vel?
+			</div>
+		</section>
+
+		<section>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorum quaerat ut veniam? Animi
+				aspernatur blanditiis, commodi dolorum error labore, nihil quae quas quia quibusdam tempore temporibus vero.
+				Animi, aperiam?
+			</div>
+			<div>Aspernatur culpa distinctio dolore et fuga, incidunt libero molestiae mollitia nostrum nulla provident quo.
+				At cum cupiditate dicta ea ex fugiat, fugit, illum incidunt laudantium maiores, quis sequi similique vel.
+			</div>
+			<div>Cumque impedit pariatur quas! Ad aliquam aspernatur delectus doloremque eveniet exercitationem id ipsam
+				ipsum, laudantium neque nisi nostrum numquam, odit officiis reprehenderit rerum sequi, sint temporibus
+				tenetur totam unde voluptates.
+			</div>
+			<div>Atque ipsa magnam nisi nobis officia praesentium repellat soluta velit? Eos ex ipsam ipsum unde veniam.
+				Autem cum dicta fugiat maiores optio unde. Dignissimos eius illum inventore, nobis officiis tempore?
+			</div>
+			<div>Adipisci eius facilis hic nam non pariatur totam. Aliquam autem dicta dolor fugiat impedit, labore
+				laboriosam libero minus molestiae mollitia, nam nulla perspiciatis provident quaerat quia quisquam sapiente
+				ut vero.
+			</div>
+			<div>Accusantium adipisci animi consequatur corporis cumque debitis dolor dolore enim eum expedita explicabo
+				facilis incidunt iusto labore natus neque nesciunt non officia perferendis praesentium quia quibusdam sunt
+				velit vero, voluptatem!
+			</div>
+			<div>A accusamus accusantium aspernatur, at, commodi cum cupiditate deserunt dignissimos eaque eligendi enim
+				facilis fuga illum itaque, laudantium minima molestiae neque nisi nostrum odit provident quis quisquam rerum
+				sequi velit!
+			</div>
+			<div>Ab accusantium alias asperiores, autem debitis dolores est expedita ipsum itaque modi necessitatibus nisi
+				provident quae quas qui ratione rem repellendus sed suscipit tenetur ut vitae, voluptatem voluptates.
+				Delectus, fugiat?
+			</div>
+			<div>Corporis cum cumque dolores eligendi ipsa obcaecati, veritatis. Consequuntur deleniti harum impedit
+				inventore, magnam natus non obcaecati officiis quo rerum. Amet at commodi dignissimos distinctio eaque
+				eveniet, ipsa provident vero!
+			</div>
+			<div>Adipisci alias architecto asperiores corporis, ducimus eligendi enim facilis id illum laboriosam magnam
+				molestias mollitia necessitatibus nesciunt numquam obcaecati pariatur, perspiciatis quisquam quod ratione
+				reprehenderit sapiente sequi soluta, tenetur vel?
+			</div>
+		</section>
+
+		<section>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorum quaerat ut veniam? Animi
+				aspernatur blanditiis, commodi dolorum error labore, nihil quae quas quia quibusdam tempore temporibus vero.
+				Animi, aperiam?
+			</div>
+			<div>Aspernatur culpa distinctio dolore et fuga, incidunt libero molestiae mollitia nostrum nulla provident quo.
+				At cum cupiditate dicta ea ex fugiat, fugit, illum incidunt laudantium maiores, quis sequi similique vel.
+			</div>
+			<div>Cumque impedit pariatur quas! Ad aliquam aspernatur delectus doloremque eveniet exercitationem id ipsam
+				ipsum, laudantium neque nisi nostrum numquam, odit officiis reprehenderit rerum sequi, sint temporibus
+				tenetur totam unde voluptates.
+			</div>
+			<div>Atque ipsa magnam nisi nobis officia praesentium repellat soluta velit? Eos ex ipsam ipsum unde veniam.
+				Autem cum dicta fugiat maiores optio unde. Dignissimos eius illum inventore, nobis officiis tempore?
+			</div>
+			<div>Adipisci eius facilis hic nam non pariatur totam. Aliquam autem dicta dolor fugiat impedit, labore
+				laboriosam libero minus molestiae mollitia, nam nulla perspiciatis provident quaerat quia quisquam sapiente
+				ut vero.
+			</div>
+			<div>Accusantium adipisci animi consequatur corporis cumque debitis dolor dolore enim eum expedita explicabo
+				facilis incidunt iusto labore natus neque nesciunt non officia perferendis praesentium quia quibusdam sunt
+				velit vero, voluptatem!
+			</div>
+			<div>A accusamus accusantium aspernatur, at, commodi cum cupiditate deserunt dignissimos eaque eligendi enim
+				facilis fuga illum itaque, laudantium minima molestiae neque nisi nostrum odit provident quis quisquam rerum
+				sequi velit!
+			</div>
+			<div>Ab accusantium alias asperiores, autem debitis dolores est expedita ipsum itaque modi necessitatibus nisi
+				provident quae quas qui ratione rem repellendus sed suscipit tenetur ut vitae, voluptatem voluptates.
+				Delectus, fugiat?
+			</div>
+			<div>Corporis cum cumque dolores eligendi ipsa obcaecati, veritatis. Consequuntur deleniti harum impedit
+				inventore, magnam natus non obcaecati officiis quo rerum. Amet at commodi dignissimos distinctio eaque
+				eveniet, ipsa provident vero!
+			</div>
+			<div>Adipisci alias architecto asperiores corporis, ducimus eligendi enim facilis id illum laboriosam magnam
+				molestias mollitia necessitatibus nesciunt numquam obcaecati pariatur, perspiciatis quisquam quod ratione
+				reprehenderit sapiente sequi soluta, tenetur vel?
+			</div>
+		</section>
+
+		<section>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto dolorum quaerat ut veniam? Animi
+				aspernatur blanditiis, commodi dolorum error labore, nihil quae quas quia quibusdam tempore temporibus vero.
+				Animi, aperiam?
+			</div>
+			<div>Aspernatur culpa distinctio dolore et fuga, incidunt libero molestiae mollitia nostrum nulla provident quo.
+				At cum cupiditate dicta ea ex fugiat, fugit, illum incidunt laudantium maiores, quis sequi similique vel.
+			</div>
+			<div>Cumque impedit pariatur quas! Ad aliquam aspernatur delectus doloremque eveniet exercitationem id ipsam
+				ipsum, laudantium neque nisi nostrum numquam, odit officiis reprehenderit rerum sequi, sint temporibus
+				tenetur totam unde voluptates.
+			</div>
+			<div>Atque ipsa magnam nisi nobis officia praesentium repellat soluta velit? Eos ex ipsam ipsum unde veniam.
+				Autem cum dicta fugiat maiores optio unde. Dignissimos eius illum inventore, nobis officiis tempore?
+			</div>
+			<div>Adipisci eius facilis hic nam non pariatur totam. Aliquam autem dicta dolor fugiat impedit, labore
+				laboriosam libero minus molestiae mollitia, nam nulla perspiciatis provident quaerat quia quisquam sapiente
+				ut vero.
+			</div>
+			<div>Accusantium adipisci animi consequatur corporis cumque debitis dolor dolore enim eum expedita explicabo
+				facilis incidunt iusto labore natus neque nesciunt non officia perferendis praesentium quia quibusdam sunt
+				velit vero, voluptatem!
+			</div>
+			<div>A accusamus accusantium aspernatur, at, commodi cum cupiditate deserunt dignissimos eaque eligendi enim
+				facilis fuga illum itaque, laudantium minima molestiae neque nisi nostrum odit provident quis quisquam rerum
+				sequi velit!
+			</div>
+			<div>Ab accusantium alias asperiores, autem debitis dolores est expedita ipsum itaque modi necessitatibus nisi
+				provident quae quas qui ratione rem repellendus sed suscipit tenetur ut vitae, voluptatem voluptates.
+				Delectus, fugiat?
+			</div>
+			<div>Corporis cum cumque dolores eligendi ipsa obcaecati, veritatis. Consequuntur deleniti harum impedit
+				inventore, magnam natus non obcaecati officiis quo rerum. Amet at commodi dignissimos distinctio eaque
+				eveniet, ipsa provident vero!
+			</div>
+			<div>Adipisci alias architecto asperiores corporis, ducimus eligendi enim facilis id illum laboriosam magnam
+				molestias mollitia necessitatibus nesciunt numquam obcaecati pariatur, perspiciatis quisquam quod ratione
+				reprehenderit sapiente sequi soluta, tenetur vel?
+			</div>
+		</section>
+	</div>
+</main>
+
+<?php
+get_footer();
+
--- template-parts/header/full.php
+++ template-parts/header/full.php
@@ -1,10 +1,43 @@
 <?php
 
 /**
- * asd
+ * Header template.
+ *
+ * @see Options -> Header.
+ *
+ * @package WordPress
+ * @subpackage inheart
  */
+
+$logo = get_field( 'header_logo_dark', 'option' );
 ?>
 
 <header class="header full">
-</header>
+	<div class="container fluid">
+		<div class="header-inner flex flex-wrap align-center">
+			<div class="header-menu">
+				<button class="header-menu-button flex align-center" type="button">
+					<span class="header-menu-button-lines">
+						<span></span>
+						<span></span>
+						<span></span>
+					</span>
+					<?php esc_html_e( 'Меню', 'inheart' ) ?>
+				</button>
+			</div>
+
+			<?php
+			if( $logo ){
+				?>
+				<div class="header-logo flex align-center">
+					<a href="<?php echo home_url( '/' ) ?>">
+						<?php echo wp_get_attachment_image( $logo['id'], 'ih-logo' ) ?>
+					</a>
+				</div>
+				<?php
+			}
+			?>
+		</div><!-- .header-inner -->
+	</div><!-- .container -->
+</header><!-- .header -->
 
