be68dab09e585ce2aeb85e0d675d36c40dffd860
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-09-04 02:09:38 +0300

    upd: return back - there was no deploy on server, no changes in files there.

--- theme-functions/new-memory-creation.php
+++ theme-functions/new-memory-creation.php
@@ -472,7 +472,6 @@ function ih_ajax_save_data_step_4(): void
 	}
 
 	update_field( 'field_638b12fcbe72b', $videos, $memory_page_id );
-	wp_die($memory_page_id . ' --- ' . print_r($videos, 1));
 
 	// External links.
 	$links = null;
