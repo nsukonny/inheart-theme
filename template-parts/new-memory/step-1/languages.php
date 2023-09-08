9ecf08c936998014352b559fa4765b7ea193164d
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-09-08 21:07:05 +0300

    feat: set lang choice in Step 1 as site lang change.

--- template-parts/new-memory/step-1/languages.php
+++ template-parts/new-memory/step-1/languages.php
@@ -15,15 +15,15 @@ $lang_uk	= ( ! $language || $language === 'uk' ) ? ' active' : '';
 ?>
 
 <div class="new-memory-langs inline-flex flex-wrap">
-	<button class="new-memory-lang<?php echo $lang_uk ?>" data-lang="uk">
+	<a href="<?php echo get_the_permalink( get_page_by_path( 'stvorennya-storinki-pamyati' )->ID ) ?>" class="new-memory-lang<?php echo $lang_uk ?>" data-lang="uk">
 		<?php esc_html_e( 'Українська', 'inheart' ) ?>
-	</button>
-	<button class="new-memory-lang<?php echo ( $language === 'ru_RU' ? ' active' : '' ) ?>" data-lang="ru_RU">
+	</a>
+	<a href="<?php echo get_the_permalink( get_page_by_path( 'sozdanie-straniczy-pamyati' )->ID ) ?>" class="new-memory-lang<?php echo ( $language === 'ru_RU' ? ' active' : '' ) ?>" data-lang="ru_RU">
 		<?php esc_html_e( 'Російська', 'inheart' ) ?>
-	</button>
-	<button class="new-memory-lang<?php echo ( $language === 'en_US' ? ' active' : '' ) ?>" data-lang="en_US">
+	</a>
+	<a href="<?php echo get_the_permalink( get_page_by_path( 'creating-a-new-memory-page' )->ID ) ?>" class="new-memory-lang<?php echo ( $language === 'en_US' ? ' active' : '' ) ?>" data-lang="en_US">
 		<?php esc_html_e( 'Англійська', 'inheart' ) ?>
-	</button>
+	</a>
 	<div class="new-memory-lang-bg"></div>
 </div>
 
