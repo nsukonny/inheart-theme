75970d604873dc92600ca70a6e591703ca8b5f66
Andrei Stezenko <andrew.stezenko@gmail.com>
2023-10-15 16:21:32 +0300

    feat: add GTag in header.

--- header.php
+++ header.php
@@ -17,36 +17,6 @@ $logo_only		= get_field( 'logo_only' );
 
 <!doctype html>
 <html class="no-js" <?php language_attributes() ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ) ?>" />
-	<meta http-equiv="x-ua-compatible" content="ie=edge" />
-	<meta content="" name="description" />
-	<meta content="" name="keywords" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-	<meta content="telephone=no" name="format-detection" />
-	<meta name="HandheldFriendly" content="true" />
-
-	<title>
-		<?php
-		global $page, $paged;
-
-		wp_title( '|', true, 'right' );
-		bloginfo( 'name' );
-		$site_description = get_bloginfo( 'description', 'display' );
-
-		if( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description";
-
-		if( $paged >= 2 || $page >= 2 ) echo ' | ' . sprintf( __( 'Page %s', 'inheart' ), max( $paged, $page ) );
-		?>
-	</title>
-
-	<!-- FAVICON -->
-	<!-- /FAVICON -->
-
-	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
-	<?php wp_head() ?>
-</head>
-
 <body <?php body_class( "theme-$page_theme $memory_theme" ) ?>>
 	<?php wp_body_open() ?>
 
@@ -55,3 +25,44 @@ $logo_only		= get_field( 'logo_only' );
 		if( $logo_only ) get_template_part( 'template-parts/header/logo-only' );
 		else get_template_part( 'template-parts/header/full' );
 		?>
+
+		<head>
+			<!-- Google tag (gtag.js) -->
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-NRBKM3VLV8"></script>
+			<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-NRBKM3VLV8');
+			</script>
+			<!-- End of Google tag (gtag.js) -->
+
+			<meta charset="<?php bloginfo( 'charset' ) ?>" />
+			<meta http-equiv="x-ua-compatible" content="ie=edge" />
+			<meta content="" name="description" />
+			<meta content="" name="keywords" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+			<meta content="telephone=no" name="format-detection" />
+			<meta name="HandheldFriendly" content="true" />
+
+			<title>
+				<?php
+				global $page, $paged;
+
+				wp_title( '|', true, 'right' );
+				bloginfo( 'name' );
+				$site_description = get_bloginfo( 'description', 'display' );
+
+				if( $site_description && ( is_home() || is_front_page() ) ) echo " | $site_description";
+
+				if( $paged >= 2 || $page >= 2 ) echo ' | ' . sprintf( __( 'Page %s', 'inheart' ), max( $paged, $page ) );
+				?>
+			</title>
+
+			<!-- FAVICON -->
+			<!-- /FAVICON -->
+
+			<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
+			<?php wp_head() ?>
+		</head>
