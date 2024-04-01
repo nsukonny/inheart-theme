c0a60dd6045419cb16359149c6211cc4ee358b45
Andrei Stezenko <andrew.stezenko@gmail.com>
2024-04-01 23:25:17 +0300

    upd: add onclick for the date picker field - 2.

--- components/inputs/date.php
+++ components/inputs/date.php
@@ -32,8 +32,8 @@ $required		= $args['required'] ?? '';
 		<?php echo ( $autocomplete ? 'autocomplete="' . esc_attr( $autocomplete ) . '"' : '' ) ?>
 		<?php echo ( $required ? 'required' : '' ) ?>
 		onfocus="this.type='date';this.showPicker()"
-		onclick="this.type='date';this.showPicker()"
-		onblur="(this.value === '' ? this.type='text' : this.type='date')"
+		onclick="this.type='date'"
+		onblur="(!this.value ? this.type='text' : this.type='date')"
 	/>
 	<span class="input-icon tail">
 		<img src="<?php echo THEME_URI ?>/static/img/calendar.svg" alt="" />
